81021f200ae2a0ee51938f358679cfdd9f745121
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-05-30 20:22:29 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+
+
+
+int main()
+{
+    int a,b;
+    while(true)
+    {
+        
+        scanf("%d %d",&a,&b);
+        
+        if(a==0 and b==0)
+        {
+            break;
+        }
+        printf("%d\n",a+b);
+    }
+    
+}
