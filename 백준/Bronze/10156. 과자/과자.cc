17b4c56bee55277665315ac3cdc55fb29394fdcd
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-05-30 20:22:04 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,16 @@
+
+#include <iostream>
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a*b<c)
+    {
+        printf("0");
+    }
+    else
+    {
+        printf("%d",a*b-c);
+    }
+
+}
