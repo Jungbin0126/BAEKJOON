59e0133652979f146836f05e34dfcf030985b13b
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-05-30 20:24:03 +0900

    [Bronze IV] Title: FYI, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cc
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n-5550000>=0 and n-5560000<0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        {
+            printf("NO");
+        }
+    }
+}
