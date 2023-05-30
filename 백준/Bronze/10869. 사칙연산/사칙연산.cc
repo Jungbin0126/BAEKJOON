2d81e7f81e7cac1951788968fad670b5f1ff0539
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-05-30 20:59:00 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b=0;
+
+    std::cin>>a>>b;
+
+    std::cout<< a + b << std::endl;
+    std::cout<< a - b << std::endl;
+    std::cout<< a * b << std::endl;
+    std::cout<< a / b << std::endl;
+    std::cout<< a % b << std::endl;
+
+
+    return 0;
+}
+
