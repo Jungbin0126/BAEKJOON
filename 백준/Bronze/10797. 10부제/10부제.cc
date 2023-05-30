e32d30d5588e8504504a2a2b1d2e9d69696d9393
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-05-30 20:22:17 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+int main()
+{
+    int a,b,c,d,e,f,total=0;
+    scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+    if(a==b)
+    {
+        total=total+1;
+    }
+    if(a==c)
+    {
+        total=total+1;
+    }
+    if(a==d)
+    {
+        total=total+1;
+    }
+    if(a==e)
+    {
+        total=total+1;
+    }
+    if(a==f)
+    {
+        total=total+1;
+    }
+    printf("%d",total);
+}
+    
+    
+
+
