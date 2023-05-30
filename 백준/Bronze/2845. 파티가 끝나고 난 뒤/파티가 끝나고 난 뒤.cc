401c93b0c7dc40a15192b2debba6561428166f2c
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-05-30 20:20:23 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,13 @@
+
+#include <iostream>
+int main()
+{
+    int a,b,c,d,e,f,g;
+    scanf("%d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g);
+    printf("%d",c-a*b);
+    printf(" %d",d-a*b);
+    printf(" %d",e-a*b);
+    printf(" %d",f-a*b);
+    printf(" %d",g-a*b);
+    
+}
