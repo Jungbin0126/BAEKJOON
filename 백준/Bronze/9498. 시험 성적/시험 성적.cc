89e11b6d70ba7775c43ba566016148951d6fa3f9
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-05-30 20:21:50 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=90)
+    {
+        printf("A");
+    }
+    else if(a>=80)
+    {
+        printf("B");
+    }
+    else if(a>=70)
+    {
+        printf("C");
+    }
+    else if(a>=60)
+    {
+        printf("D");
+    }
+    else if(a<=59)
+    {
+        printf("F");
+    }
+}
