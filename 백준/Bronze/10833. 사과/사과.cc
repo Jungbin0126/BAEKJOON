b77333184713e24ac848cd848ce60f52987d5051
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-06-18 09:19:05 +0900

    [Bronze III] Title: 사과, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n=0;
+    
+    cin>>n;
+    int a,b,sum=0;
+
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+
+        if(a<b){
+            sum+=b%a;
+        }
+        else if(a>b){
+            sum+=b;
+        }
+    }
+
+    cout<<sum;
+
+
+
+    }
