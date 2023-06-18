8019c322ec0ab27360c24bbe29a4dae6af080a31
Jungbin0126 <114058281+Jungbin0126@users.noreply.github.com>
2023-06-18 09:25:11 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n=0;
+    
+    cin>>n;
+    int a;
+
+    for(int i=0;i<n;i++){
+        cin>>a;
+
+        for(int j=0; j<a; j++){
+            cout<<"=";
+        }
+
+        cout<<'\n';
+
+       
+    }
+
+
+
+
+
+    }
